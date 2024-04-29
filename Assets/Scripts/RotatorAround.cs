341488880ef44107044906fcaa484da6992dcd93
writel1ne <70217377+writel1ne@users.noreply.github.com>
2024-04-29 12:08:53 +0300

    .

---
+++ Assets/Scripts/RotatorAround.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+	[SerializeField, Range(0, 100)] private float _rotationOffset = 0.1f;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, _rotationOffset, 0) * Time.fixedDeltaTime);
+    }
+}
--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -6,8 +6,8 @@ public class Scaler : MonoBehaviour
 {
 	[SerializeField, Range(0, 0.1f)] private float _scaleOffset = 0;
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-		gameObject.transform.localScale += new Vector3(_scaleOffset, _scaleOffset, _scaleOffset);
+		transform.localScale += Vector3.one * _scaleOffset;
     }
 }
