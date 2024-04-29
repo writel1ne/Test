1ec9092a68f73ea1cc87d7fc34581a9c8433fd2b
writel1ne <70217377+writel1ne@users.noreply.github.com>
2024-04-29 12:25:58 +0300

    .

--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Scaler : MonoBehaviour
 {
-	[SerializeField, Range(0, 0.1f)] private float _scaleOffset = 0;
+	[SerializeField, Range(0, 1f)] private float _scaleOffset = 0;
 
     private void FixedUpdate()
     {
-		transform.localScale += Vector3.one * _scaleOffset;
+		transform.localScale += Vector3.one * _scaleOffset * Time.fixedDeltaTime;
     }
 }
